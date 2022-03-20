d2a7191a3f012d68417d01bcdda6e6f6e7a442b7
CruZer0 <luca-wolckenhauer@outlook.de>
2022-03-20 16:35:41 +0100

    fixed installation issue and changed template text

--- src/web/views.py
+++ src/web/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from tcore.models import TempLog
 
 def temp_view(request):
-    db = TempLog.objects.get(id=2)
-    context = {
-        'db' : db,
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 # Create your views here.
